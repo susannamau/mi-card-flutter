4da44461bdaa70b7773617e43f0e6da7f6cd25af
susannamau <maugeri.susanna@gmail.com>
2023-10-19 23:17:28 +0200

    Dependencies errors fixed

---
+++ android/app/src/main/kotlin/com/example/momentaneo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.momentaneo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red,
         body: Container(),
       ),
     ),
